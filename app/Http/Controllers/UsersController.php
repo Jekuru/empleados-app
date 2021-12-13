cd2d399c02b21118aba4649cda195647eb6098e9
Jekuru <FJOAndreu@hotmail.es>
2021-12-13 11:55:16 +0100

    Comprobar email valido y rol (enum) valido

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,28 +31,48 @@ class UsersController extends Controller
         }
         $user->api_token = Str::random(60);
 
+        // COMPROBAR ENUM
+        if($user->role == 'users' || $user->role == 'hr' || $user->role == 'directive'){
+            $checkRole = true;
+        } else {
+            $checkRole = false;
+        }
+
+        // COMPROBAR SI SE HA INTRODUCIDO UN EMAIL VALIDO
+        $checkEmail = $this->CheckEmail($user->email);
+
         // COMPROBAR SI LA CONTRASEÑA ES SEGURA
-        $safe = $this->CheckPassword($user->password);
+        $checkPassword = $this->CheckPassword($user->password);
 
         // COMPROBAR SI EL EMAIL YA ESTÁ REGISTRADO
         $exists = User::where('email', '=', $data->email)->first();
         
         try {
-            if($safe){
-                if($exists){
-                    $msg['msg'] = "No se pudo crear el usuario especificado, ya existe un usuario con el email: ".$user->email;
-                    $msg['status'] = 2;
+            if($checkRole){
+                if($checkEmail){
+                    if($checkPassword){
+                        if($exists){
+                            $msg['msg'] = "No se pudo crear el usuario especificado, ya existe un usuario con el email: ".$user->email;
+                            $msg['status'] = 2;
+                        } else {
+                            $user->save();
+                            $msg['msg'] = "Nuevo usuario registrado con email ".$user->email;
+                            $msg['status'] = 1;
+                        }
+                    } else {
+                        $msg['msg'] = "La contraseña no es segura";
+                        $msg['status'] = 3;
+                    }
                 } else {
-                    $user->save();
-                    $msg['msg'] = "Nuevo usuario registrado con email ".$user->email;
-                    $msg['status'] = 1;
+                    $msg['msg'] = "Por favor, introduce una dirección de correo valida";
+                    $msg['status'] = 4;
                 }
             } else {
-                $msg['msg'] = "La contraseña no es segura";
-                $msg['status'] = 3;
+                $msg['msg'] = "No se ha especificado un rol valido";
+                $msg['status'] = 5;
             }
         } catch(\Exception $e){
-            $msg['msg'] = $e->getmsg();
+            $msg['msg'] = $e->getMessage();
             $msg['status'] = 0;
         }
 
@@ -63,10 +83,22 @@ class UsersController extends Controller
      * Comprobar si la contraseña es segura
      * Param: $password -> string
      */
-    public function CheckPassword($password){
+    function CheckPassword($password){
         $pattern = '/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{6,}/';
 
         if(preg_match($pattern, $password)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Comprobar si se ha introducido un email válido
+     * Param: $email -> string
+     */
+    function CheckEmail($email){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
             return true;
         } else {
             return false;
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['users', 'hr', 'directive']);
+            $table->enum('role', ['users', 'hr', 'directive'])->default('users');
             $table->integer('salary');
             $table->string('biography')->nullable()->default(null);
             $table->rememberToken();
